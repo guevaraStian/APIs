472bf34ab3b1c2b872ee901f924488a4c4ae72e3
unknown <sebasguesa@hotmail.com>
2025-02-20 15:04:13 -0500

    Api que consulta en internet unas imagenes aleatoriay cada refrescada de la pagina trae una imagen nueva

---
+++ ImagenesEnPantallaJS/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world')
+
+const URL = 'https://api.thecatapi.com/v1/images/search';
+
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    });
+
+
+
+
+
+
